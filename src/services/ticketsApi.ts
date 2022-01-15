0dce8b2dd4fe969a41b565963880010f6692dba8
Askhat <s.askhat.b@gmail.com>
2022-01-15 21:19:39 +0600

    delete unused

--- src/services/ticketsApi.ts
+++
@@ -1,7 +0,0 @@
-export const getTickets = async () => {
-  const search = await fetch(`http://localhost:1234/search`);
-  const searchResponse = await search.json();
-  await fetch(
-    `http://localhost:1234/tickets?searchId=${searchResponse?.searchId}`
-  ).then((res) => res.json());
-};
--- src/util/transfers.ts
+++
@@ -1,7 +0,0 @@
-module.exports = Object.freeze({
-  ALL: "Все",
-  WITHOUT: "Без пересадок",
-  ONE: "1 пересадка",
-  TWO: "2 пересадки",
-  THREE: "3 пересадки",
-});
